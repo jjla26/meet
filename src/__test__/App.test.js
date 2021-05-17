e4b8c4ac1c44138a706759ab7919c8dde0f09ecf
Julio Lopez <julioj.lopeza@gmail.com>
2021-05-17 23:06:14 +0200

    Added test for app component to show citysearch component

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../components/app/App';
 import EventList from '../components/event-list/EventList';
+import CitySearch from '../components/citysearch/CitySearch';
 
 describe('<App /> component', () => {
+  let AppWrapper;
+  beforeAll(() => {
+    AppWrapper = shallow(<App />);
+  })
   test('render list of events', () => {
-    const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(EventList)).toHaveLength(1);
   })
+
+  test('render CitySearch', () => {
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
 });
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import EventList from '../event-list/EventList'
+import CitySearch from '../citysearch/CitySearch'
 
 function App() {
   return (
     <div className="App">
       <EventList />
+      <CitySearch />
     </div>
   );
 }

d2ecd9090e4e0a0f02ffa1620ec501eddc9ace8d
Julio Lopez <julioj.lopeza@gmail.com>
2021-05-18 11:36:22 +0200

    Added Event component with test

---
+++ src/__test__/Event.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Event from '../components/event/Event';
+import { mockData } from "../mock-data/mock-data";
+
+describe('<Event /> component', () => {
+  let event 
+  let EventWrapper
+  beforeAll(() => {
+    event = mockData[0];
+    EventWrapper = shallow(<Event event={event} />)
+  })
+
+  test('should have a collapsed event details by default', () => {
+    expect(EventWrapper.find('.event__details')).toHaveLength(1);
+    expect(EventWrapper.find('.show__details')).toHaveLength(0);
+  })
+
+  test('should show event details when click on button', () => {
+    EventWrapper.find('.event__button').simulate('click')
+    expect(EventWrapper.find('.show__details')).toHaveLength(1);
+  })
+
+  test('should hide event details when click on button', () => {
+    EventWrapper.state({
+      show: true
+    })
+    EventWrapper.find('.event__button').simulate('click')
+    expect(EventWrapper.find('.show__details')).toHaveLength(0);
+  })
+
+  test('should show all the data of the events', () => {
+    expect(EventWrapper.find('.event__summary')).toHaveLength(1);
+    expect(EventWrapper.find('.event__location')).toHaveLength(1);
+    expect(EventWrapper.find('.event__description')).toHaveLength(1);
+    expect(EventWrapper.find('.event__organizer')).toHaveLength(1);
+    expect(EventWrapper.find('.event__start')).toHaveLength(1);
+    expect(EventWrapper.find('.event__end')).toHaveLength(1);
+  })
+});
--- src/components/event/Event.js
+++ src/components/event/Event.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+  state = {
+    show: false
+  }
+
+  handleClick = () => {
+    this.setState( (prevState, props) => ({ show: !prevState.show}))
+  }
+
   render() {
-    return <div></div>;
+    return (
+      <div className="event">
+        <h2 className="event__summary"></h2>
+        <h4 className="event__location"></h4>
+        <button className="event__button" onClick={this.handleClick}>See events details</button>
+        <ul className={`event__details ${this.state.show ? 'show__details': ''}`}>
+          <li className="event__description"></li>
+          <li className="event__organizer"></li>
+          <li className="event__start"></li>
+          <li className="event__end"></li>
+        </ul>
+
+      </div>
+    );
   }
 }
 export default Event;

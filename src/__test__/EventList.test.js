8a11f45df5e0a8b5fec77021d9262e042f47144d
Julio Lopez <julioj.lopeza@gmail.com>
2021-05-18 09:06:50 +0200

    Added mockdata

--- src/__test__/EventList.test.js
+++ src/__test__/EventList.test.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import EventList from '../components/event-list/EventList';
 import Event from '../components/event/Event';
+import { mockData } from "../mock-data/mock-data";
 
 describe('<EventList /> component', () => {
   test('render the correct number of events', () => {
-    const EventListWrapper = shallow(<EventList events={[{ id:1 },{ id:2 },{ id:3 }]} />);
-    expect(EventListWrapper.find(Event)).toHaveLength(3);
+    const EventListWrapper = shallow(<EventList events={mockData} />);
+    expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
   })
 });

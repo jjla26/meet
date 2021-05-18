2b5a12b67416297e4abbf50b70ab3e40901f8f9e
Julio Lopez <julioj.lopeza@gmail.com>
2021-05-18 13:51:11 +0200

    Added mockdata to eventlist component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import EventList from '../event-list/EventList'
 import CitySearch from '../citysearch/CitySearch'
 import NumberOfEvents from '../NumberOfEvents/NumberOfEvents'
+import { mockData } from '../../mock-data/mock-data'
 
 function App() {
   return (
     <div className="App">
-      <EventList />
+      <EventList events={mockData} />
       <CitySearch />
       <NumberOfEvents />
     </div>

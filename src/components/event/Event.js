746537781106e0bbaa99c5fb32e1ae459e9be51a
Julio Lopez <julioj.lopeza@gmail.com>
2021-05-19 12:31:40 +0200

    Added data to the event component

--- src/components/event/Event.js
+++ src/components/event/Event.js
@@ -12,14 +12,15 @@ class Event extends Component {
   render() {
     return (
       <div className="event">
-        <h2 className="event__summary"></h2>
-        <h4 className="event__location"></h4>
+        <h2 className="event__summary">{this.props.event.summary}</h2>
+        <p className="event__date">{`${new Date(this.props.event.start.dateTime)}`}</p>
+        <h4 className="event__location">{this.props.event.location}</h4>
         <button className="event__button" onClick={this.handleClick}>See events details</button>
-        <ul className={`event__details ${this.state.show ? 'show__details': ''}`}>
-          <li className="event__description"></li>
-          <li className="event__organizer"></li>
-          <li className="event__start"></li>
-          <li className="event__end"></li>
+        <ul className={`event__details ${this.state.show ? '': 'display-none'}`}>
+          <li className="event__description">Description: {this.props.event.description}</li>
+          <li className="event__organizer">Organizer: {this.props.event.organizer.email}</li>
+          <li className="event__start">Start: {`${new Date(this.props.event.start.dateTime)} - ${this.props.event.start.timeZone}`}</li>
+          <li className="event__end">End: {`${new Date(this.props.event.end.dateTime)} - ${this.props.event.start.timeZone}`}</li>
         </ul>
 
       </div>

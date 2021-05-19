241a26e0b6d8dec65f939415bff290d049a82359
Julio Lopez <julioj.lopeza@gmail.com>
2021-05-19 12:31:57 +0200

    Added test to show and hide the list of suggestions

--- src/__test__/CitySearch.test.js
+++ src/__test__/CitySearch.test.js
@@ -63,4 +63,20 @@ describe('<CitySearch /> component', () => {
     CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
     expect(CitySearchWrapper.state("query")).toBe(suggestions[0]);
   });
+
+  test("selecting CitySearch input reveals the suggestion list", () => {
+    CitySearchWrapper.find('.city').simulate('focus');
+    expect(CitySearchWrapper.state('showSuggestions')).toBe(true);
+    expect(CitySearchWrapper.find('.showSuggestions')).toHaveLength(1);
+  })
+
+  test("selecting a suggestion should hide the suggestions list", () => {
+    CitySearchWrapper.setState({
+      query: 'Berlin',
+      showSuggestions: undefined
+    });
+    CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
+    expect(CitySearchWrapper.state('showSuggestions')).toBe(false);
+    expect(CitySearchWrapper.find('.showSuggestions')).toHaveLength(0);
+  });
 });

ab7eec3714045330d9efba25a2a428392e904025
Julio Lopez <julioj.lopeza@gmail.com>
2021-05-21 15:27:48 +0200

    Added end2end test when the user types in the input

--- src/__test__/EndToEnd.test.js
+++ src/__test__/EndToEnd.test.js
@@ -1,4 +1,6 @@
-import puppeteer from "puppeteer";
+import puppeteer, { Keyboard } from "puppeteer";
+import { mockData } from '../mock-data/mock-data';
+import { extractLocations } from '../api/api';
 
 describe('show/hide an event details', () => {
   let browser;
@@ -30,4 +32,35 @@ describe('show/hide an event details', () => {
     const eventDetails = await page.$('.event__details .display-none');
     expect(eventDetails).toBeNull();
   });
+});
+
+describe('Filter events by city', () => {
+  let browser;
+  let page;
+  let locations;
+  jest.setTimeout(30000);
+  beforeAll(async () => {
+    browser = await puppeteer.launch({ slowMo: 250, headless: false});
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.city');
+    locations = extractLocations(mockData);
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+  
+  test('the user should see the list of upcoming events.', async () => {
+    const event = await page.$('.event')
+    expect(event).toBeDefined()
+  });
+
+  test('should show a list of suggestion when the user types', async () => {
+    await page.focus('.city')
+    await page.keyboard.type('Berlin')
+    let suggestions = await page.$$('.suggestions li')
+    expect(suggestions).toHaveLength(2)
+  });
+
 });

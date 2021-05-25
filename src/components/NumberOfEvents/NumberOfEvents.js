7fc5a0ca7db23cc220978b01daa49f0233377b7b
Julio Lopez <julioj.lopeza@gmail.com>
2021-05-25 22:56:41 +0200

    Added alert messages

--- src/components/NumberOfEvents/NumberOfEvents.js
+++ src/components/NumberOfEvents/NumberOfEvents.js
@@ -1,15 +1,26 @@
 import React, { useState } from 'react'
 
+import { ErrorAlert } from '../alert/Alert'
+
 function NumberOfEvents(props) {
-  const  [ numberOfEvents, setNumberOfEvents ] = useState(props.numberOfEvents)
+  const [ numberOfEvents, setNumberOfEvents ] = useState(props.numberOfEvents)
+  const [ error, setError ] = useState('')
 
   const handleChange = (e) => {
-    setNumberOfEvents(e.target.value)
-    props.updateNumberOfEvents(e.target.value)
+    if(e.target.value >= 1 && e.target.value <= 32){
+      setNumberOfEvents(e.target.value)
+      props.updateNumberOfEvents(e.target.value)
+      setError('')
+    }else{
+      setNumberOfEvents(e.target.value)
+      props.updateNumberOfEvents('')
+      setError('Select a number from 1 to 32')
+    }
   }
 
   return (
     <div className="NumberOfEvents">
+      <ErrorAlert text={error} />
       <label>Nº. of events</label>
       <input
         type="number"
---
+++ src/components/alert/Alert.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+class Alert extends Component {
+  constructor(props) {
+    super(props);
+    this.color = null;
+  }
+
+  getStyle = () => {
+    return {
+      color: this.color,
+    };
+  }
+
+  render() {
+    return (
+      <div className="Alert">
+        <p style={this.getStyle()}>{this.props.text}</p>
+      </div>
+    );
+  }
+}
+
+class InfoAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = '#bbe1fa';
+  }
+}
+
+class WarningAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'yellow';
+  }
+}
+
+class ErrorAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color= 'red'
+    this.getStyle = () => {
+      return {
+        position: 'absolute',
+        top: '150px',
+        right: '0px',
+        width: '200px',
+        color: this.color
+      }
+    }
+  }
+}
+
+export { InfoAlert, ErrorAlert, WarningAlert }
